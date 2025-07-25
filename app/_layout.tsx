7f99ef056112e96414993e6b11bf2bc2435813dd
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-07-25 17:50:20 +0200

    feat: update app layout and remove details screen; add jsEngine to app.json

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,5 +3,25 @@ import '../global.css';
 import { Stack } from 'expo-router';
 
 export default function Layout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{
+          headerShown: false,
+          title: 'Stride',
+          headerTitleStyle: {
+            fontSize: 24,
+            fontWeight: 'bold',
+          },
+        }}
+      />
+      <Stack.Screen
+        name="auth"
+        options={{
+          headerShown: false,
+        }}
+      />
+    </Stack>
+  )
 }
