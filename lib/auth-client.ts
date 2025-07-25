97df30f3e0fbaa93ce0ef2bc82266c23bee7c537
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-07-25 21:52:32 +0200

    [ADD]: Correct

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -4,7 +4,7 @@ import { magicLinkClient } from "better-auth/client/plugins";
 import * as SecureStore from "expo-secure-store";
 
 export const authClient = createAuthClient({
-    baseURL: process.env.NODE_ENV === "production" ? "https://api.stride.buzz" : "http://localhost:8080",
+    baseURL: process.env.NODE_ENV === "production" ? "https://api.stride.buzz" : "http://192.168.1.2:8080",
     plugins: [
         expoClient({
             scheme: "stride",
